5abddab2c784bc88dcae391aced9b4540e56fab3
larsnovikov <larsnovikov@yandex.ru>
2018-03-18 10:09:44 +0300

    hot fix

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ mkdir images/watermark
 sudo docker-compose down;
 sudo docker-compose up -d --build;
 
-sudo docker exec -ti cdn_php-fpm_1 /bin/bash init.sh
+sudo docker exec -ti dockercdn2_php-fpm_1 /bin/bash init.sh
 
 sudo bash nginx_app/init.sh
 sudo bash nginx_front/init.sh
