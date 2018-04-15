a003bfa719cac5e1a21c162219b6a38007904a6f
larsnovikov <larsnovikov@yandex.ru>
2018-04-15 10:32:55 +0300

    Возможность установить колво воркеров в супервизоре при запуске

--- php-fpm/init.sh
+++ php-fpm/init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+read -p "Enter proces num for queue listener[5]: " proc_count
+proc_count=${proc_count:-5}
+
+sudo docker exec -ti dockercdn2_php-fpm_1 sed -i -e "s/proc_count/$proc_count/g" /etc/supervisor/supervisord.conf
+
 sudo docker exec -ti dockercdn2_php-fpm_1 supervisord -c /etc/supervisor/supervisord.conf
 exit;
